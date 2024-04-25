83e21127c4326262cd83ac11b7346d982094cb74
Shivam-jn <shivamjain892@gmail.com>
2024-04-25 20:36:13 +0530

    Some changes

--- api/index.js
+++ api/index.js
@@ -8,14 +8,16 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log('connected to MongoDB');
-})
-.catch((err)=>{
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
 const app = express();
 
@@ -23,26 +25,26 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, ()=>{
-  console.log('Server is running on port  3000');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/user",userRouter); 
-
+app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname,'client/dist')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success : false,
+    success: false,
     statusCode,
     message,
   });
